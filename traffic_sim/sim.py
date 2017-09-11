201d77e9d6870d857e8b445357190989254f55fd
Elbert Fliek <elbert@crowdynews.com>
2017-09-11 17:29:29 +0200

    made car start location variable

--- traffic_sim/sim.py
+++ traffic_sim/sim.py
@@ -14,7 +14,7 @@ HEIGHT = 480
 FRAMERATE = 60
 
 
-class PyManMain:
+class SimMain:
     """The Main PyMan Class - This class handles the main
     initialization and creating of the Game."""
 
@@ -30,13 +30,8 @@ class PyManMain:
             self.width, self.height))
 
     def MainLoop(self):
-        """This is the Main Loop of the Game"""
-
         """Load All of our Sprites"""
         self.LoadSprites()
-        """tell pygame to keep sending up keystrokes when they are
-        held down"""
-        pygame.key.set_repeat(500, 30)
 
         """Create the background"""
         self.background = pygame.Surface(self.screen.get_size())
@@ -44,35 +39,34 @@ class PyManMain:
         self.background.fill((0, 0, 0))
 
         while 1:
-            self.car.move()
+            for car in self.cars_sprites:
+                car.move()
 
-            """Do the Drawging"""
+            """Do the Drawing"""
             self.screen.blit(self.background, (0, 0))
 
-            self.car_sprites.draw(self.screen)
+            self.cars_sprites.draw(self.screen)
             pygame.display.flip()
             time.sleep(1.0 / FRAMERATE)
 
     def LoadSprites(self):
-        """Load the sprites that we need"""
-        self.car = Car()
-        self.car_sprites = pygame.sprite.RenderPlain((self.car))
+        self.cars_sprites = pygame.sprite.Group()
+        self.cars_sprites.add(Car(pygame.Rect(0, HEIGHT / 2, 64, 64)))
 
 
 class Car(pygame.sprite.Sprite):
     """This is our car that will move around the screen"""
 
-    def __init__(self):
+    def __init__(self, rect=None):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image('car_small.png', -1)
+        if rect is not None:
+            self.rect = rect
         """Set the number of Pixels to move each time"""
         self.x_dist = 5
         self.y_dist = 5
 
     def move(self):
-        """Move your self in one of the 4 directions according to key"""
-        """Key is the pyGame define for either up,down,left, or right key
-        we will adjust outselfs in that direction"""
         xMove = 0
         yMove = 0
 
@@ -81,5 +75,5 @@ class Car(pygame.sprite.Sprite):
 
 
 if __name__ == "__main__":
-    MainWindow = PyManMain()
+    MainWindow = SimMain()
     MainWindow.MainLoop()
