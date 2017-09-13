cd5d0d6b25f55afc7cf0815522321f470865c68b
Noel Luneburg <noel.luneburg@target-holding.nl>
2017-09-13 23:21:35 +0200

    Fixed code in carlogic.

--- traffic_sim/carlogic.py
+++ traffic_sim/carlogic.py
@@ -1,48 +1,45 @@
+from intersection import Lane
+
+
 class Vehicle:
 
- 	def __init__(self, length: int, width: int, vehicleType: str, speed: int, turn: int, lane: str, maxSpeed: int, acceleration: int, id: int):
-    	self.length = length
-    	self.width = width
-	    self.vehicleType = vehicleType
-	    self.speed = speed
-	    self.brakeSpeed = brakeSpeed
-	    self.turn = turn
-	    self.id = id
-	    self.lane = lane
-	    self.maxSpeed = maxSpeed
-	    self.acceleration = acceleration
+    def __init__(self, length: int, width: int, vehicleType: str, speed: int, turn: int, lane: Lane, maxSpeed: int,
+                 acceleration: int, brakeSpeed: int, id_: int):
+        self.length = length
+        self.width = width
+        self.vehicleType = vehicleType
+        self.speed = speed
+        self.brakeSpeed = brakeSpeed
+        self.turn = turn
+        self.id = id_
+        self.lane = lane
+        self.maxSpeed = maxSpeed
+        self.acceleration = acceleration
 
-  def frameUpdate(self):
-      if (not lane.checkLight):
-          speed = max(speed - brakeSpeed,0)
-      if(lane.checkLight):
-          speed = min(speed + acceleration ,maxSpeed)
-
-      return(speed)
+    def frameUpdate(self):
+        if (not self.lane.checklight()):
+            self.speed = max(self.speed - self.brakeSpeed, 0)
+        else:
+            self.speed = min(self.speed + self.acceleration, self.maxSpeed)
 
 
 class TrafficLight:
 
-  def __init__(self,green: bool, strategy: str, id: int):
-      self.green = green
-      self.id = id
-      self.strategy = strategy
-      self.framerateCount = 0
+    def __init__(self, green: bool, strategy: str, id_: int):
+        self.green = green
+        self.id = id_
+        self.strategy = strategy
+        self.framerateCount = 0
+        self.changeRate = 100
 
-  if (self.strategy = classic):
-      self.changeRate = 180
+        if (self.strategy == 'classic'):
+            self.changeRate = 180
 
+    def frameUpdate(self):
+        self.framerateCount += 1
+        if (self.changeRate <= self.framerateCount):
+            self.framerateCount = 0
+            self.changeLight()
 
-  def frameUpdate(self):
-      self.framerateCount += 1
-      if (changerate=<framerateCount)
-          framerateCount = 0
-          self.changeLight()
-
-  def changeLight(self):
-    self.green = not green
-
-
-
-
-
+    def changeLight(self):
+        self.green = not self.green
