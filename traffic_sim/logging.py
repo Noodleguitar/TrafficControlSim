36f324dd433eb02ccc06f5c820ea8a1c5b8cc60f
AlbertThie <alb0rt@gmail.com>
2017-10-05 16:55:30 +0200

    Create logging.py

---
+++ traffic_sim/logging.py
@@ -0,0 +1,9 @@
+
+def init():
+    global westCount = 0
+    global eastCount = 0
+    global northCount = 0
+    global southCount = 0
+
+def getTotal():
+  return (westCount + eastCount + northCount + southCount)
